91ee29bcea06ad20d46815174d273232d16e2213
Paul_Luque <pluquec@unsa.edu.pe>
2024-06-19 01:29:03 -0500

    copy beacon scanner from lab

---
+++ app/src/main/java/com/danp/artexploreapp/services/BeaconScannerService.kt
@@ -0,0 +1,101 @@
+package com.danp.artexploreapp.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import android.util.Log
+import com.idnp2024a.beaconscanner.BeaconScanerLibrary.Beacon
+
+class BeaconScannerService : Service() {
+    private val TAG = "BeaconScannerService"
+    private val binder = MyBinder()
+
+    private var currentGallery: String? = null
+    private var nearestPainting: String? = null
+
+
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "OnCreate BeaconScannerService")
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy BeaconScannerService")
+
+
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+        Log.d(TAG, "onStartCommand BeaconScannerService")
+    }
+    override fun onBind(intent: Intent?): IBinder? {
+        return binder
+    }
+
+
+    // Metodo publicos
+    fun getCurrentGallery(): String? {
+        return currentGallery
+    }
+
+    // Metodos publicos
+    fun getNearestPainting(): String? {
+        return nearestPainting
+    }
+
+    // metodos auxiciliares
+    private fun getGalleryFromMajor(major: Int?): String {
+        // Implementa la lógica para mapear Major a galerías
+        return when (major) {
+            1 -> "Galería A"
+            2 -> "Galería B"
+            3 -> "Galería C"
+            else -> "Desconocido"
+        }
+    }
+
+    private fun getPaintingFromMinor(major: Int, minor: Int): String {
+        // Implementa la lógica para mapear Minor a pinturas específicas dentro de una galería
+        return "Pintura $minor en Galería $major"
+    }
+
+    private fun handleBeacons(beacons: Collection<Beacon>) {
+        // Implementa la lógica para determinar la galería y la pintura más cercana
+        // Ejemplo simplificado:
+        var closestBeacon: Beacon? = null
+        var closestDistance = Double.MAX_VALUE
+        for (beacon in beacons) {
+            val distance = beacon.distance
+            if (distance != null) {
+                if (distance < closestDistance) {
+                    closestDistance = distance
+                    closestBeacon = beacon
+                }
+            }
+        }
+
+        closestBeacon?.let {
+            val major = it.major
+            val minor = it.minor
+            if(major!= null && minor!=null){
+                currentGallery = getGalleryFromMajor(major)
+                nearestPainting = getPaintingFromMinor(major, minor)
+            }
+        }
+    }
+
+
+
+    // Clase interna que extiende Binder y define métodos públicos del servicio
+    inner class MyBinder : Binder() {
+        fun getService(): BeaconScannerService {
+            return this@BeaconScannerService
+        }
+    }
+}
---
+++ app/src/main/java/com/danp/artexploreapp/services/utilsIBeacon/BeaconScanerLibrary/Beacon.kt
@@ -0,0 +1,45 @@
+package com.idnp2024a.beaconscanner.BeaconScanerLibrary
+
+class Beacon(
+    val macAddress: String?,
+    var manufacturer: String? = null,
+    var type: BeaconType = BeaconType.ANY,
+    var uuid: String? = null,
+    var major: Int? = null,
+    var minor: Int? = null,
+    var namespace: String? = null,
+    var instance: String? = null,
+    var rssi: Int? = null,
+    var txPower: Int? = null,
+    var distance: Double? = 0.0
+) {
+    enum class BeaconType {
+        IBEACON, EDDYSTONE_UID, ANY
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Beacon) return false
+        if (uuid != other.uuid) return false
+        return true
+    }
+
+//    private val movingAverageFilter = MovingAverageFilter(5)
+    fun calculateDistanceAverageFilter(txPower: Int, rssi: Int, n: Double): Double? {
+        return MovingAverageFilter.calculateDistance(txPower, rssi, n)
+    }
+
+    override fun hashCode(): Int {
+        return uuid?.hashCode() ?: 0
+    }
+    override fun toString(): String {
+        return "Beacon(macAddress=$macAddress, manufacturer=$manufacturer, type=$type, uuid=$uuid, major=$major, minor=$minor, rssi=$rssi, TxPower=$txPower)"
+    }
+
+    fun calculateDistance(txPower: Int, rssi: Int, N: Double): Any {
+        val factor = (txPower - rssi) / (10 * N)
+        val distance = Math.pow(10.0, factor)
+        return distance;
+
+    }
+}
