00e1476c7c3083b9411adc7eec837189166fc647
PoolLuque <pluquec@unsa.edu.pe>
2024-07-31 21:22:43 -0500

    Se mejoro la presentacion del scanner

--- app/src/main/java/com/danp/artexploreapp/qr/presentation/QrScreen.kt
+++ app/src/main/java/com/danp/artexploreapp/qr/presentation/QrScreen.kt
@@ -4,22 +4,29 @@ import android.net.Uri
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.danp.artexploreapp.R
 import com.danp.artexploreapp.paiting.presentation.PaintingsViewModel
+import com.danp.artexploreapp.ui.theme.SecondaryColor
 import com.danp.artexploreapp.util.MyTopBar
 import com.google.gson.Gson
 import com.journeyapps.barcodescanner.ScanContract
@@ -32,7 +39,8 @@ fun QrScreen(navController: NavController, paintingsViewModel: PaintingsViewMode
     // Registrar el lanzador y el manejador de resultados
     val barcodeLauncher = rememberLauncherForActivityResult(ScanContract()) { result ->
         if (result.contents == null) {
-            Toast.makeText(context, "Cancelled", Toast.LENGTH_LONG).show()
+            // Mensaje mejorado para el usuario
+            Toast.makeText(context, "Escaneo cancelado. Por favor, intenta de nuevo.", Toast.LENGTH_LONG).show()
         } else {
             val scannedId = result.contents
             val painting = paintingsViewModel.getPaintingById(scannedId)
@@ -42,40 +50,83 @@ fun QrScreen(navController: NavController, paintingsViewModel: PaintingsViewMode
                 Log.i("EJEM", painting.toString())
                 val paintingJson = gson.toJson(painting)
                 navController.navigate("paintingView/${Uri.encode(paintingJson)}")
-
-//            navController.navigate("paintingView/${painting.id}")
             } else {
-                Toast.makeText(context, "No se encontró la pintura con ID: $scannedId", Toast.LENGTH_LONG).show()
+                // Mensaje más descriptivo si no se encuentra la pintura
+                Toast.makeText(context, "No se encontró la pintura con ID: $scannedId. Por favor, verifica el código QR.", Toast.LENGTH_LONG).show()
             }
         }
     }
 
     Scaffold(
-        topBar = { MyTopBar(navController = navController, header = "QR", isHome = false) }
+        topBar = { MyTopBar(navController = navController, header = "Escanear QR", isHome = false) }
     ) { ip ->
 
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(ip)
-                .wrapContentSize(Alignment.Center)
+                .padding(ip),
+            verticalArrangement = Arrangement.Center, // Centra verticalmente
+            horizontalAlignment = Alignment.CenterHorizontally // Centra horizontalmente
         ) {
+            // Envolviendo la imagen en un Box con clickable
+            Box(
+                modifier = Modifier
+                    .size(300.dp)
+                    .padding(12.dp)
+                    .clickable {
+                        // Iniciar el escáner cuando se toca la imagen
+                        val options = ScanOptions()
+                        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE) // Solo escanea códigos QR
+                        options.setPrompt("Por favor, coloca el código QR en el centro de la pantalla para escanear")
+                        options.setBeepEnabled(true)
+                        options.setCaptureActivity(CaptureActivityPortrait::class.java)
+                        options.setOrientationLocked(false)
+
+                        barcodeLauncher.launch(options)
+                    }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.scan_qr),
+                    contentDescription = "Escanear Código QR",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    contentScale = ContentScale.Fit
+                )
+            }
+
             Text(
-                text = "QR Screen"
+                text = "Escáner QR",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(16.dp)
+            )
+
+            Text(
+                text = "Escanea un código QR para descubrir más información de la obra de arte.",
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
             )
 
             // Botón para iniciar el escaneo
-            Button(onClick = {
-                val options = ScanOptions()
-                options.setDesiredBarcodeFormats(ScanOptions.QR_CODE) // Solo escanea códigos QR
-                options.setPrompt("Scan a QR code")
-                options.setBeepEnabled(true)
-                options.setCaptureActivity(CaptureActivityPortrait::class.java)
-                options.setOrientationLocked(false)
+            Button(
+                onClick = {
+                    val options = ScanOptions()
+                    options.setDesiredBarcodeFormats(ScanOptions.QR_CODE) // Solo escanea códigos QR
+                    options.setPrompt("Por favor, coloca el código QR en el centro de la pantalla para escanear")
+                    options.setBeepEnabled(true)
+                    options.setCaptureActivity(CaptureActivityPortrait::class.java)
+                    options.setOrientationLocked(false)
 
-                barcodeLauncher.launch(options)
-            }) {
-                Text("Scan QR Code")
+                    barcodeLauncher.launch(options)
+                },
+                colors = ButtonDefaults.buttonColors(containerColor = SecondaryColor),
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text("Escanear Código QR", color=Color.White, fontSize = 18.sp)
             }
         }
     }
